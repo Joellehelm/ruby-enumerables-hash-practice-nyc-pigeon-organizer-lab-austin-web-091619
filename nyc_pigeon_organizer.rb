0b9b28b5d1274d0e6b1e57b24cbafdac9613eb7a
Joelle Helm <joellenhelm@gmail.com>
2019-09-01 20:59:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,34 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+  data.each do |k, col|
+    col.each do |lives, v|
+      v.each do |name|
+        pigeon_list[name] = {:color => [], :gender => 0 , :lives => 0}
+
+  end
+
+end
+end
+
+    data[:gender].each do |gender, names|
+      names.each do |name|
+        pigeon_list[name][:gender] = [gender.to_s]
+      end
+  end
+
+    data[:lives].each do |home, names|
+      names.each do |name|
+        pigeon_list[name][:lives] = [home]
+      end
+    end
+
+    data[:color].each do |color, names|
+      names.each do |name|
+        pigeon_list[name][:color] << color.to_s
+      end
+    end
+
+  return pigeon_list
 end
